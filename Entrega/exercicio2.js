97b2e57235872a459ad531b06f15ec267c207a8a
Maria <meduardalima88@gmail.com>
2020-08-17 11:41:52 -0300

    exercicio 2 concluido

---
+++ Entrega/exercicio2.js
@@ -0,0 +1,3 @@
+let temperaturaFahrenheit = prompt("Digite a temperatura em Fahrenheit")
+let temperaturaCelsius = ((temperaturaFahrenheit - 32) / 1.8)
+console.log("A temperatura em Celsius eh: " + temperaturaCelsius)
