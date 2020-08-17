3cf0a76556f829f0306364b264b4d95a0d6503f9
Valeria Melo <valleriademelo@gmail.com>
2020-08-17 04:03:17 -0300

    Resolution exercise 4

---
+++ Entrega/Exercicio4.js
@@ -0,0 +1,13 @@
+//4 - Escreva um programa que pergunte a quantidade de km percorridos por um carro alugado pelo
+//usuário, assim como a quantidade de dias pelos quais o carro foi alugado. Calcule o preço a pagar, sabendo que o carro custa R$ 60,00 por dia e R$ 0,15 por km rodado.
+
+let day = parseFloat(
+  prompt('Informe a quantidade de dias de locação do veiculo: ')
+);
+let kilometerTraveled = parseFloat(
+  prompt('Informe quantos kilometros pretende percorrer com o veiculo: ')
+);
+let kilometerRunResult = kilometerTraveled * 0.15;
+let ValueDays = day * 60;
+let priceToPay = kilometerRunResult + ValueDays;
+console.log(' O valor total a pagar é de ' + 'R$ ' + priceToPay + '.');
