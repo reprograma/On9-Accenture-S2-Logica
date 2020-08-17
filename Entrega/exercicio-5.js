71d8e653b1a1a5185546b3be25be1f103cbf0d14
Jennyffer de Morais <jennyffernovaes@hotmail.com>
2020-08-17 16:11:32 -0300

    adicionar resolução do exercício 5

--- Entrega/exercicio-5.js
+++ Entrega/exercicio-5.js
@@ -2,3 +2,22 @@
 
 /* Faça um Programa que peça os três lados de um triângulo. Nenhum valor digitado pode ser 0 (zero) ou negativo. Indique, caso os lados formem um triângulo, se o mesmo é: equilátero, isósceles ou escaleno. */
 
+let primeiroLado = Number(prompt("Digite o tamanho do 1º lado do triângulo: "));
+let segundoLado = Number(prompt("Digite o tamanho do 2º lado do triângulo: "));
+let terceiroLado = Number(prompt("Digite o tamanho do 3º lado do triângulo: "));
+
+function classificarTriangulo() {
+  if (primeiroLado == segundoLado && segundoLado == terceiroLado) {
+    alert("Triângulo EQUILÁTERO!");
+  } else if (
+    primeiroLado != segundoLado &&
+    primeiroLado != terceiroLado &&
+    segundoLado != terceiroLado
+  ) {
+    alert("Triângulo ESCALENO!");
+  } else {
+    alert("Triângulo ISÓSCELES!");
+  }
+}
+
+classificarTriangulo();
