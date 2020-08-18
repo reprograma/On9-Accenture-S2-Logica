45193166dd7644bf09ec77a9d8ec59e4d809c8ab
vivian costa <vivianalessandra_@hotmail.com>
2020-08-18 20:49:20 -0300

    commit exercicio 2

---
+++ Entrega/exercicio2.js
@@ -0,0 +1,10 @@
+
+//2 - Faça agora o contrário, de Fahrenheit para Celsius.
+
+let fahrenheit = Number(prompt("insira uma temperatura em fahrenheit"));
+
+let celsius = (fahrenheit-32)*5/9
+
+console.log("temperatura em celsius: " + celsius.toFixed(4))
+
+
