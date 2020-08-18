5a3903fb2c108361271ab7f363bd74550f39d682
Helena Flor <helenafernandes619@gmail.com>
2020-08-18 12:27:37 -0300

    resoluçao exercicio 1

---
+++ Entrega/conversorCelsiusParaFahrenheit.js
@@ -0,0 +1,5 @@
+function converterCelsiusFahrenheit(valorEmCelsius){
+    return 9*valorEmCelsius/5 + 32
+} 
+const valorEmCelsius = Number(prompt("informe o valor em graus Celsius: "))
+converterCelsiusFahrenheit(valorEmCelsius)
