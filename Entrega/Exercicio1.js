b0c7e6601d507a3401abc2de280b3b7facd86c93
Milena <milenaalmeida_@hotmail.com>
2020-08-18 09:35:41 -0300

    Adicionando o exercício 1

---
+++ Entrega/Exercicio1.js
@@ -0,0 +1,3 @@
+let temperaturaCelsius =  prompt ("Olá, esse programinha irá converter temperaturas de Celsius para Fahrenheit. Digite a sua temperatura em Celsius:");
+let formulaConversao = ( (9 * Number(temperaturaCelsius)/5)  + (32));
+alert = ("A temperatura que você digitou em Celsius equivale a :"  + formulaConversao + " Fahrenheit.");
