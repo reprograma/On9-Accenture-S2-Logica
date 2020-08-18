05ada6620c4fc8cf1c2284ce741fbdc813b939b3
Ana Reis <anareis1706@gmail.com>
2020-08-17 23:40:48 -0300

    Exercício 1 e Exercício 2

---
+++ Entrega/Exercício2.js
@@ -0,0 +1,5 @@
+let tempF = prompt ("Qual a temperatura em Fahrenheit(ºF) para conversão em Celsius(ºC) ?")
+let tempC = (5/9)*(tempF-32)
+
+alert("A temperatura convertida em Celsius é: " + tempC + "ºC" )
+console.log("A temperatura convertida em Celsius é: " + tempC + "ºC" )
