558c48621d376132f763d339ce8c6f645e7f941e
Fabí <fabicaneyu@gmail.com>
2020-08-19 17:07:12 -0300

    atualizando exercicio 2

--- Entrega/ExerciciosFabi/exercicio2.js
+++ Entrega/ExerciciosFabi/exercicio2.js
@@ -2,8 +2,13 @@
 //2 - Faça agora o contrário, de Fahrenheit para Celsius.
 //(32 °F − 32) × 5/9 = 0 °C
 
-
+//opção 1
 let valor = prompt("Qual a temperatura está no momento em Fahrenheit?")
 let conta = ((Number(valor)- 32) * 5)/9
 
 console.log("A temperatura em graus Celcius é "+conta)
+
+
+//opção 2
+let valor = (Number(prompt("Qual a temperatura está no momento em Fahrenheit?"))-32)*5/9
+console.log("A temperatura em graus Celcius é "+valor.toFixed(0))
