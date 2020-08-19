c87b45f9c2f27b9ab84ad7a824ec404ca77b8bd0
Gabriela <gabrielamgp01@gmail.com>
2020-08-18 23:00:30 -0300

    Exercicios 1 e 2

---
+++ Entrega/exercicio1.js
@@ -0,0 +1,5 @@
+var celsius = Number( prompt("Temperatura em graus Celsius: "));
+
+var fahrenheit = ((9*celsius/5) + 32).toFixed(2) ;
+
+console.log=("Temperatura em graus Fahrenheit: " + fahrenheit);
