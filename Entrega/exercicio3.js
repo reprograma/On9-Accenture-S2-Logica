ea39a8c736c36788c0a4b14f162b01fbe914d9be
vivian costa <vivianalessandra_@hotmail.com>
2020-08-18 21:35:48 -0300

    commit do exercicio 3

---
+++ Entrega/exercicio3.js
@@ -0,0 +1,18 @@
+
+//exercicio 3
+
+let pesoPeixe = Number(prompt("escreva, em kg, quanto você pescou hoje."));
+   
+if(pesoPeixe <=50) {        
+        multa=0
+        excessoDePeso=0
+            console.log("peso da pesca dentro das normas. " + excessoDePeso + "kg e multa de R$" + multa + ".")
+    }
+
+if(pesoPeixe >50) {
+        excessoDePeso = (pesoPeixe)-50  
+        multa = excessoDePeso*4
+            console.log("peso da pesca excedente. " + excessoDePeso + "kg a mais do permitido. multa de R$" + multa + ".")
+
+}
+
