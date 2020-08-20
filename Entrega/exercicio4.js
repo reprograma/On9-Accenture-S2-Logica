b966906cc1183f5ef8775cb334fc445d017a68bd
Gabriela <gabrielamgp01@gmail.com>
2020-08-19 23:13:34 -0300

    exercicio 4 calculo diária

---
+++ Entrega/exercicio4.js
@@ -0,0 +1,10 @@
+var aluguelDias= Number (prompt("Quantos dias você vai utilizar o carro? "))   
+
+var kmPercorridos = Number (prompt("Quantos KM você percorreu? "))      
+
+var valorDiariaseKm= aluguelDias * 60 + kmPercorridos
+
+console.log=("O valor total a pagar é R$: " + valorDiariaseKm )
+
+
+
