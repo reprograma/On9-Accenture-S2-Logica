8cd46d5a9be77b04ed0734a213a1b60178097362
Fabi <fabicaneyu@gmail.com>
2020-08-20 23:50:13 -0300

    exercício 1 vscode com Node

---
+++ Entrega/ExerciciosFabi/exercicioNode1.js
@@ -0,0 +1,8 @@
+//Enunciado:
+//1 - Converta uma temperatura digitada em Celsius para Fahrenheit. 
+// Dica: a formula do Fahrenheit = 9*Celsius/5 + 32
+
+var celsius = 32
+
+var farenheit = console.log("A temperatura "+celsius+"°C, transformada em Farenheit é "+((9 * celsius/5) + 32)+" °F")
+
