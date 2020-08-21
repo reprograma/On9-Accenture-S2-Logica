810a1985314a97cdb94047ab88872e8e52cd909b
bruna-anjos <bruninhaandrade360@gmail.com>
2020-08-20 23:42:38 -0300

    Resolução do exer2.js

--- Entrega/exer1.js
+++ Entrega/exer1.js
@@ -2,5 +2,7 @@
 //1 - Converta uma temperatura digitada em Celsius para Fahrenheit.
 //Dica: a formula do Fahrenheit = 9*Celsius/5 + 32
 
-let conta = Number(prompt("Qual a temperatura está no momento em graus Celsius?"))*9/5+32
-console.log("A temperatura em Fahrenheit é "+conta)
+let celsius = prompt("Qual a temperatura está no momento em graus celsius?")
+let fahrenheit = ((9*celsius)/5)+32   
+
+console.log("A temperatura em Fahrenheit é: "+ fahrenheit)
