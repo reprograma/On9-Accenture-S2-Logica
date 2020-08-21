aad0f47140aa2027b2ba9beab6bab57f36adf84c
Valeria Melo <valleriademelo@gmail.com>
2020-08-20 22:23:34 -0300

    switchCase

---
+++ Entrega/Exercicio9.js
@@ -0,0 +1,6 @@
+//Calculo Velocidade media
+
+var distance = Number(prompt('Qual a distancia a ser percorrida, em km'));
+var velocity = Number(prompt('Qual a velocidade media, em km? '));
+
+var timeTravel = distance / velocity;
