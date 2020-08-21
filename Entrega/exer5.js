2716dc18a7c43fa6a85426cd0b1749a45b90fe76
bruna-anjos <bruninhaandrade360@gmail.com>
2020-08-21 01:23:14 -0300

    Resoluçao exer5.js

--- Entrega/exer5.js
+++ Entrega/exer5.js
@@ -2,15 +2,22 @@
 //digitado pode ser 0 (zero) ou negativo. Indique, caso os lados formem um
 //triângulo, se o mesmo é: equilátero, isósceles ou escaleno.
 
-let lado1 = parseInt(prompt("Digite o primeiro lado do triângulo:"));
-let lado2 = parseInt(prompt("Digite o segundo lado do triângulo:"));
-let lado3 = parseInt(prompt("Digite o terceiro lado do triângulo:"));
+let lado1 = prompt("Digite o primeiro lado do triângulo:")
+let lado2 = prompt("Digite o segundo lado do triângulo:")
+let lado3 = prompt("Digite o terceiro lado do triângulo:")
 
-if ((lado1 < (lado2 + lado3)) && (lado2 < (lado1 + lado3)) && (lado3 < (lado1 + lado2))) {
-    
-console.log("Os valores podem formar um triângulo.");
-}  
-else {
-    
-console.log("Os valores não podem formar um triângulo.");
+
+
+if (lado1 == lado2 && lado2 == lado3){ 
+
+    console.log("Triângulo equilatero")
+}
+
+else if ((lado1 == lado2 && lado2 != lado3)||(lado1 != lado2 && lado2 == lado3)||(lado1 == lado3 && lado2 != lado3)){
+
+    console.log("Triângulo isosceles")
+}
+
+else { console.log("Triângulo escaleno")
+
 }
