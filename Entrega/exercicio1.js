89fcdd69146f3d3328e3b1523d033ecfa2c33440
raphaelaromao <raphaelaromaot@gmail.com>
2020-08-22 18:10:47 -0300

    Adiciono os exercicios 1, 2 e 4

---
+++ Entrega/exercicio1.js
@@ -0,0 +1,5 @@
+exercicio1
+var celsius = Number(prompt('Digite a temperatura em Celsius'))
+var conversao = 9 * (celsius/5) + 32
+console.log ('A temperatura final convertida em Fahrenheit é: ' +conversao)
+
