20c7f3cb566549beb5483636481b95c38b184fa8
araujoaline <aline.daff@gmail.com>
2020-08-22 16:24:39 -0300

    Exercicio do triangulo maligno.

--- Entrega/exercicio-05.js
+++ Entrega/exercicio-05.js
@@ -1,6 +1,3 @@
-/**Faça um Programa que peça os três lados de um triângulo. 
- * Nenhum valor digitado pode ser 0 (zero) ou negativo. Indique, 
- * caso os lados formem um triângulo, se o mesmo é: equilátero, isósceles ou escaleno. */
 window.alert("Medida diferente de ZERO");
 
 var ladoUm = Number(prompt("Medida de lado 1: "));
