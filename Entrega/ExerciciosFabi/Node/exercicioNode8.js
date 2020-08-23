9058c0521006f1d4ec56e44e83c71d31cffd9bf2
Fabi <fabicaneyu@gmail.com>
2020-08-22 23:53:16 -0300

    Add exercício 8 vscode com Node

---
+++ Entrega/ExerciciosFabi/Node/exercicioNode8.js
@@ -0,0 +1,11 @@
+//Extra
+//8 - Ano bissexto: receba um ano pelo prompt e imprima no console se ele é bissexto ou não. 
+
+
+var ano = 2020
+
+if( ano % 4 === 0){
+    console.log("Este é um ano bissexto")
+} else {
+    console.log("Este não é um ano bissexto")
+}
