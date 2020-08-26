116e4b33be46e8e0c8cee6fffda994d17cca53e0
Jennyffer de Morais <jennyffernovaes@hotmail.com>
2020-08-26 12:10:23 -0300

    ajustar exercício 3

--- Entrega/exercicio-3.js
+++ Entrega/exercicio-3.js
@@ -4,12 +4,15 @@
 
 let peso = Number(prompt("Digite o peso da pesca de hoje: "));
 
+let excesso;
+let multa;
+
 if (peso > 50) {
-  let excesso = peso - 50;
-  let multa = (excesso * 4).toFixed(2);
+  excesso = peso - 50;
+  multa = (excesso * 4).toFixed(2);
   alert("Pagar multa de R$ " + multa);
 } else {
-  let excesso = 0;
-  let multa = 0;
+  excesso = 0;
+  multa = 0;
   alert("Excesso = " + excesso + " e Multa = " + multa);
 }
